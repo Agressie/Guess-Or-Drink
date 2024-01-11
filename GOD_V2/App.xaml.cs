8fdb429c2a7f885874f820d11f3a2cb7b842e726
Agressie <noah@lenkens.nl>
2024-01-11 13:12:58 +0100

    Add project files.

---
+++ GOD_V2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GOD_V2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ GOD_V2/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace GOD_V2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
